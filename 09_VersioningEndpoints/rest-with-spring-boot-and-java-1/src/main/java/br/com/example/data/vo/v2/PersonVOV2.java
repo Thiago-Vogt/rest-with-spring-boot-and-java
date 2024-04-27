cb0546cb15a985602bbbbfc1d1ef0c4fa655907e
Thiago Vogt <63065574+Thiago-Vogt@users.noreply.github.com>
2024-04-27 14:34:41 -0300

    VO Pattern and Versioning Endpoints

---
+++ 09_VersioningEndpoints/rest-with-spring-boot-and-java-1/src/main/java/br/com/example/data/vo/v2/PersonVOV2.java
@@ -0,0 +1,124 @@
+package br.com.example.data.vo.v2;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PersonVOV2 implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+
+	private long id;
+	
+	private String firstName;
+	private String lastName;
+	private String addres;
+	private String gender;
+	private Date birthday;
+	
+	public PersonVOV2() {
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return addres;
+	}
+
+	public void setAddress(String addres) {
+		this.addres = addres;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((addres == null) ? 0 : addres.hashCode());
+		result = prime * result + ((birthday == null) ? 0 : birthday.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonVOV2 other = (PersonVOV2) obj;
+		if (addres == null) {
+			if (other.addres != null)
+				return false;
+		} else if (!addres.equals(other.addres))
+			return false;
+		if (birthday == null) {
+			if (other.birthday != null)
+				return false;
+		} else if (!birthday.equals(other.birthday))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+	
+}
